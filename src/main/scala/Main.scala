40f45fd9ea6555db00c11ba880837ae8371ba778
AnthonyGrod <tantien02@hotmail.com>
2023-05-19 02:47:03 +0200

    Changed error handling

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,44 +10,42 @@ import java.io.IOException
 object WikipediaScraper {
   
 	def main(args: Array[String]): Unit = {
-		try {
-			val links = JsoupScraper.getLinks(args)
-			println(links)
-
-
-		} catch {
-			case e: IOException => println("Site does not exist.")
-		}
+		val links = JsoupScraper.getLinks(args)
+    links match {
+      case Right(links) => links.foreach(println)
+      case Left(errorMessage) => println(s"Error: $errorMessage")
+    }
 	}
 }
 
 
 object JsoupScraper {
 
-	@throws(classOf[IOException])
-  def getLinks(args: Array[String]): Buffer[String] = {
+  def getLinks(args: Array[String]): Either[String, Buffer[String]] = {
 		// TODO: better handling of command line arguments
 		if (args.length < 2) {
-      println("Usage: JsoupScraper <languageCode> <articleName>")
-      return Buffer.empty[String]
+      Left("Usage: JsoupScraper <languageCode> <articleName>")
     }
 
-    val languageCode = args(0)
-    val articleName = args(1)
+		try {
+			val languageCode = args(0)
+			val articleName = args(1)
 
-		// Wikipedia uses % to encode foreign characters in article names. Also, spaces are replaced with underscores.
-		val articleLinksPattern: Regex = s"(https://$languageCode\\.wikipedia.org/wiki/[a-zA-Z0-9%_()]+)".r
+			// Wikipedia uses % to encode foreign characters in article names. Also, spaces are replaced with underscores.
+			val articleLinksPattern: Regex = s"(https://$languageCode\\.wikipedia.org/wiki/[a-zA-Z0-9%_()]+)".r
 
-		val doc = Jsoup.connect(s"https://$languageCode.wikipedia.org/wiki/$articleName").get()
-		val links = doc.select("a[href]")
-			.asScala
-      .map(link => link.attr("abs:href"))
-			.collect(
-				{
-					case articleLinksPattern(url) => url
-				}
-			)
-
-		return links
+			val doc = Jsoup.connect(s"https://$languageCode.wikipedia.org/wiki/$articleName").get()
+			val links = doc.select("a[href]")
+				.asScala
+				.map(link => link.attr("abs:href"))
+				.collect(
+					{
+						case articleLinksPattern(url) => url
+					}
+				)
+			Right(links)
+		} catch {
+			case e: IOException => Left("Site does not exist.")
+		}
   }
 }
